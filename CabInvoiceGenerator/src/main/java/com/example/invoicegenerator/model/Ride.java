08d373e018697f08f191d64bca0a807fcf0ae14b
Vyshakh Jayan <vyshakh.jayan@thoughtworks.com>
2019-10-15 02:35:13 +0530

    Code refactor

--- CabInvoiceGenerator/src/main/java/com/example/invoicegenerator/model/Ride.java
+++ CabInvoiceGenerator/src/main/java/com/example/invoicegenerator/model/Ride.java
@@ -11,14 +11,7 @@ public class Ride {
         this.rideType = rideType;
     }
 
-    private static final int MINIMUM_DISTANCE_FARE = 10;
-    private static final int MINIMUM_DURATION_FARE = 1;
-    private static final int MINIMUM_FARE = 5;
-
     public double fare() {
-        if(this.rideType.equals(RideType.PREMIUM))
-            return RideType.PREMIUM.fare(distance, duration);
-        else
-            return RideType.NORMAL.fare(distance, duration);
+        return rideType.fare(distance, duration);
     }
 }
